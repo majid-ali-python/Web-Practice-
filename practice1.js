65b633316f8f540a49fcd7f095c91e1d77c6bd46
majid-ali-python <majidali121251@gmail.com>
2026-09-05 20:55:43 +0500

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,9 @@
+//Take a user input a number using prompt ("Enter a Number") .check if number is multiple of 5  or not
+let num = prompt("Enter a Number")
+if (num%5===0){
+    console.log(" Your Number Is Multiple Of 5")
+
+}  
+else{
+    console.log("Your Number Is Not Multiple of 5")
+}
