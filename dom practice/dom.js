c9b025573ad0431c8684809fdaa23e7e461a66f6
majid-ali-python <majidali121251@gmail.com>
2026-09-10 16:37:18 +0500

    Add files via upload

---
+++ dom practice/dom.js
@@ -0,0 +1,6 @@
+// kisi element ko access kar k us ki valuse ko change karna
+
+let h1 = document.querySelector("h1");
+console.dir(h1.innerText);
+
+h1.innerText = h1.innerText + "Kia hal hai majid ali";
